3a418d2816f4a35a1182eb7600f6f57f7c8d86f9
IgorVinogradov1 <igor.vinogradov1@yandex.com>
2025-11-16 01:08:27 +0300

    Update lesson6.py

--- lesson6.py
+++ lesson6.py
@@ -1,30 +1,31 @@
-password = input('Введите пароль: ')
+def main():
 
-def is_very_long(password):
-    result = len(password) >= 12
-    return result
-def has_digit(password):
-	return any(symbol.isdigit() for symbol in password)
-def has_lower_letters(password):
-	return any(symbol.islower() for symbol in password)
-def has_upper_letters(password):
-	return any(symbol.isupper() for symbol in password)
-def has_symbols(password):
-    return any(not (symbol.isdigit() or symbol.isalpha()) for symbol in password)
-def rating_calculation():
-    score = 0
-    checklist = [
-        is_very_long(password),
-        has_digit(password),
-        has_lower_letters(password),
-        has_upper_letters(password),
-        has_symbols(password)
-    ]
-    for i in range(len(checklist)):
-    	if checklist[i]:
-            score += 2
-    print('Рейтинг пароля: ', score)
-rating_calculation()
+    password = input('Введите пароль: ')
+    def is_very_long(password):
+        result = len(password) >= 12
+        return result
+    def has_digit(password):
+	    return any(symbol.isdigit() for symbol in password)
+    def has_lower_letters(password):
+	    return any(symbol.islower() for symbol in password)
+    def has_upper_letters(password):
+	    return any(symbol.isupper() for symbol in password)
+    def has_symbols(password):
+        return any(not (symbol.isdigit() or symbol.isalpha()) for symbol in password)
+    def rating_calculation():
+        score = 0
+        checklist = [
+            is_very_long(password),
+            has_digit(password),
+            has_lower_letters(password),
+            has_upper_letters(password),
+            has_symbols(password)
+        ]
+        for i in range(len(checklist)):
+    	    if checklist[i]:
+                score += 2
+        print('Рейтинг пароля: ', score)
+    rating_calculation()
 
-
-
+if __name__ == '__main__':
+    main()
