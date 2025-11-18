45c5efa64d13adb5430d0abc862df5eb43bf7325
IgorVinogradov1 <igor.vinogradov1@yandex.com>
2025-11-18 17:52:32 +0300

    Добавил по 2 пропуска строки между функциями

--- lesson6.py
+++ lesson6.py
@@ -1,14 +1,24 @@
 def is_very_long(password):
     result = len(password) >= 12
     return result
+
+
 def has_digit(password):
-	return any(symbol.isdigit() for symbol in password)
+    return any(symbol.isdigit() for symbol in password)
+
+
 def has_lower_letters(password):
-	return any(symbol.islower() for symbol in password)
+    return any(symbol.islower() for symbol in password)
+
+
 def has_upper_letters(password):
-	return any(symbol.isupper() for symbol in password)
+    return any(symbol.isupper() for symbol in password)
+
+
 def has_symbols(password):
     return any(not (symbol.isdigit() or symbol.isalpha()) for symbol in password)
+
+
 def rating_calculation(password):
     score = 0
     checklist = [
@@ -19,13 +29,15 @@ def rating_calculation(password):
         has_symbols(password)
     ]
     for i in range(len(checklist)):
-    	if checklist[i]:
+        if checklist[i]:
             score += 2
     return score
+
 
 def main():
     password = input('Введите пароль: ')
     rating = rating_calculation(password)
     print('Рейтинг пароля: ', rating)
+
 if __name__ == '__main__':
     main()
